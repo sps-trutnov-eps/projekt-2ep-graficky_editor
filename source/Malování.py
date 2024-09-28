4b9cf84db7fe81d13ba561e4d2881fb500c994e1
prokopecm23 <prokopecm23@spstrutnov.cz>
2024-09-28 20:42:48 +0200

    štětec pohyb

--- source/Malování.py
+++ source/Malování.py
@@ -1,11 +1,13 @@
 import sys
 import pygame
 import os
+from pygame.locals import *
 
 pygame.init
 window_size = 1200, 900
 window = pygame.display.set_mode(window_size)
 
+brush1 = pygame.Rect(50, 50, 50, 50)
 while True:
     
     window.fill((255,255,255))
@@ -13,5 +15,9 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+    
+    brush1.center = pygame.mouse.get_pos()
+    
+    pygame.draw.rect(window, (0, 0, 255), brush1)
     pygame.display.flip()
 
