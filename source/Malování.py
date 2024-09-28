9638e7324b3b38da85f995241f8b200a07a4f135
prokopecm23 <prokopecm23@spstrutnov.cz>
2024-09-28 20:33:45 +0200

    začátek part 2

--- source/Malování.py
+++ source/Malování.py
@@ -3,6 +3,15 @@ import pygame
 import os
 
 pygame.init
-window = pygame.display.set_mode(1200, 900)
+window_size = 1200, 900
+window = pygame.display.set_mode(window_size)
 
+while True:
+    
+    window.fill((255,255,255))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.flip()
 
