32cbee6e85e8f8dabb5155f7c421cdb0956645d3
Vojtěch Synáč <synacv23@spstrutnov.cz>
2024-11-10 23:38:20 +0100

    nevim co v tom kodu v predchozim commitu bylo za veci
    ctrl+c a ctrl+v posledniho commitu od prokopecm23

--- source/Malování.py
+++ source/Malování.py
@@ -3,7 +3,7 @@ import pygame
 import os
 from pygame.locals import *
 
-pygame.init()
+pygame.init
 window_size = 1500, 900
 window = pygame.display.set_mode(window_size)
 gui = pygame.Rect(0, 0, 300, 900)
@@ -14,14 +14,6 @@ red_button = pygame.Rect(165, 285, 110, 110)
 green_button = pygame.Rect(25, 420, 110, 110)
 
 
-<<<<<<< HEAD
-brush_size = 50  # počáteční velikost štětce
-canvas = pygame.Surface(window_size)
-canvas.fill((255, 255, 255))
-
-brush_color1 = (0, 0, 255)
-
-=======
 brush_size = 50
 brush1 = pygame.Rect(50, 50, brush_size, brush_size)
 brush_preview = pygame.Rect(50, 50, brush_size, brush_size)
@@ -29,34 +21,16 @@ canvas = pygame.Surface(window_size)
 canvas.fill((255, 255, 255))
 
 brush_color = (0, 0, 255)
->>>>>>> origin/main
 while True:
+    
     mouse_pressed = pygame.mouse.get_pressed()
     mouse_pos = pygame.mouse.get_pos()
     stisknute = pygame.key.get_pressed()
-
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-<<<<<<< HEAD
-            
-    # Aktualizace pozice a velikosti štětce
-    brush_rect = pygame.Rect(0, 0, brush_size, brush_size)
-    brush_rect.center = mouse_pos
-
-    # Kreslení na plátno
-    if mouse_pressed[0]:  # levé tlačítko myši
-        pygame.draw.ellipse(canvas, brush_color1, brush_rect)
-    
-    # Změna velikosti štětce pomocí + a -
-    if stisknute[pygame.K_PLUS] or stisknute[pygame.K_KP_PLUS]:  # přidání i klávesnice s numerickými klávesami
-        brush_size += 1
-    if stisknute[pygame.K_MINUS] or stisknute[pygame.K_KP_MINUS]:
-        brush_size = max(1, brush_size - 1)  # velikost štětce nemůže být menší než 1
-
-    # Vykreslení na obrazovku
-=======
     
         
     brush_preview.center = mouse_pos
@@ -88,9 +62,9 @@ while True:
     if stisknute[pygame.K_g]:
         brush_color = (0, 255, 0)
     
->>>>>>> origin/main
     window.blit(canvas, (0, 0))
-    pygame.draw.ellipse(window, brush_color1, brush_rect)  # náhled štětce v aktuální velikosti a pozici
+    pygame.draw.ellipse(window, (0, 0, 255), brush1)
+    pygame.draw.ellipse(window, (0, 0, 0), brush_preview, width=2)
     pygame.draw.rect(window, (200, 200, 200), gui)
     pygame.draw.rect(window, (175, 175, 175), size_slider)
     pygame.draw.rect(window, (0, 0, 255), blue_button)
@@ -98,4 +72,3 @@ while True:
     pygame.draw.rect(window, (0, 255, 0), green_button)
     pygame.draw.rect(window, (0, 0, 0), black_button)
     pygame.display.flip()
-
